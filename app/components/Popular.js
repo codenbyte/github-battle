51aba8c000f167bf2dc895de728507a22105f87d
Jen Vat <jennyvat@yahoo.com>
2017-07-11 14:48:15 -0600

    first commit

---
+++ app/components/Popular.js
@@ -0,0 +1,54 @@
+var React = require('react');
+var PropTypes = require('prop-types');
+
+function SelectLanguage (props) {
+  var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
+  return (
+    <ul className='languages'>
+      {languages.map(function (lang) {
+        return (
+          <li
+            style={lang === props.selectedLanguage ? {color: '#d0021b'} : null}
+            onClick={props.onSelect.bind(null, lang)}
+            key={lang}>
+              {lang}
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+SelectLanguage.propTypes = {
+  selectedLanguage: PropTypes.string.isRequired,
+  onSelect: PropTypes.func.isRequired,
+};
+
+class Popular extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      selectedLanguage: 'All',
+    };
+
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+  updateLanguage(lang) {
+    this.setState(function () {
+      return {
+        selectedLanguage: lang,
+      }
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SelectLanguage
+          selectedLanguage={this.state.selectedLanguage}
+          onSelect={this.updateLanguage} />
+      </div>
+    )
+  }
+}
+
+module.exports = Popular;
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path');
+// var SRC_DIR = path.resolve(__dirname, "app")
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+// template: SRC_DIR + '/index.html'
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.(js)$/, use: 'babel-loader' },
+      { test: /\.(css)$/, use: [ 'style-loader', 'css-loader' ]}
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'app/index.html'
+    })
+  ]
+};
