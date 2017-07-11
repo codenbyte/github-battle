3ed85987a6ad57e51e59b2b4c342d9f74702ae1e
Jen Vat <jennyvat@yahoo.com>
2017-07-11 16:15:01 -0600

    repos display

---
+++ app/utils/api.js
@@ -0,0 +1,17 @@
+var axios = require('axios');
+
+module.exports = {
+  fetchPopularRepos: function (language) {
+     var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'+ language + '&sort=stars&order=desc&type=Repositories');
+
+     return axios.get(encodedURI)
+      .then(function (res) {
+        return res.data.items;
+      });
+  }
+}
+
+// fetchPopularRepos('Java')
+//   .then(function (res) {
+//
+//   })
